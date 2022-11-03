b33ef2fbaa1e99dc3760fd0ea72641ec4a58f1eb
Rohit Garg <rohit.garg@meesho.com>
2022-11-03 09:11:37 +0530

    Initial commit

---
+++ src/main/java/poc/experimentation/springhbase/pocspringhbase/PocSpringHbaseApplication.java
@@ -0,0 +1,13 @@
+package poc.experimentation.springhbase.pocspringhbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringHbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringHbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poc/experimentation/springhbase/pocspringhbase/PocSpringHbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.experimentation.springhbase.pocspringhbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringHbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
