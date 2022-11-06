89ca52607f0db43f87f923ae522daff84017054d
Rohit Garg <rohit.garg@meesho.com>
2022-11-06 19:57:57 +0530

    create default table

--- src/main/java/poc/experimentation/springhbase/pocspringhbase/controller/CRUDController.java
+++ src/main/java/poc/experimentation/springhbase/pocspringhbase/controller/CRUDController.java
@@ -1,7 +1,9 @@
 package poc.experimentation.springhbase.pocspringhbase.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import poc.experimentation.springhbase.pocspringhbase.service.CRUDService;
 
@@ -16,16 +18,26 @@ public class CRUDController {
         this.crudService = crudService;
     }
 
-    @GetMapping(value = "/api/v1/getAllTables")
+    @GetMapping(value = "/api/v1/getAllTables", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity listTables() {
         try {
-            crudService.listTables();
-            return ResponseEntity.ok().build();
-        }
-        catch (Exception e){
+            return ResponseEntity.ok().body(crudService.listTables());
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
 
         }
     }
+
+    @PostMapping(value = "/api/v1/createDefaultTable", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity createDefaultTable() {
+        try {
+            crudService.createDefaultTable();
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+
+        }
+    }
+
 
 }
--- src/main/java/poc/experimentation/springhbase/pocspringhbase/service/CRUDService.java
+++ src/main/java/poc/experimentation/springhbase/pocspringhbase/service/CRUDService.java
@@ -1,12 +1,18 @@
 package poc.experimentation.springhbase.pocspringhbase.service;
 
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.TableDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import poc.experimentation.springhbase.pocspringhbase.model.HBaseConnection;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -15,9 +21,26 @@ public class CRUDService {
     @Autowired
     private HBaseConnection connection;
 
-    public void listTables() throws IOException {
+    public List<String> listTables() throws IOException {
         TableName table = TableName.valueOf("default", "user_audience_map");
         Connection conn = this.connection.getConnection();
-        System.out.println(conn.getTable(table));
+        Admin admin = conn.getAdmin();
+        List<TableDescriptor> tableDescriptorList = admin.listTableDescriptors();
+        List <String> tableList = tableDescriptorList.stream().map(td ->td.getTableName().getNameAsString()).collect(Collectors.toList());
+        return tableList;
+    }
+
+    public void createDefaultTable() throws IOException {
+        Connection conn = this.connection.getConnection();
+        Admin admin =  conn.getAdmin();
+        TableName table = TableName.valueOf("default", "table1");
+        String family1 = "family1";
+        String family2 = "family2";
+
+        HTableDescriptor desc = new HTableDescriptor(table);
+        desc.addFamily(new HColumnDescriptor(family1));
+        desc.addFamily(new HColumnDescriptor(family2));
+        admin.createTable(desc);
+
     }
 }
